e95f7106300a8ba3392842411707f9de70160da6
H3R01A <aallen0001@gmail.com>
2025-03-26 17:00:29 -0700

    update prompt for spelling error and remove Digimon

--- lib/lambdas/queryBedrock/index.ts
+++ lib/lambdas/queryBedrock/index.ts
@@ -69,7 +69,7 @@ export const handler = async (event: any): Promise<any> => {
     const prompt = `You are an expert in financial services and providing personal finance advice to everyone. 
     Your task is to analyze and summarize information and make it easy for people to understand.
     
-    Here is some information. Please make it easier to understand using Pokemo and Digimon analogies:
+    Here is some information. Please make it easier to understand using Pokemon analogies:
     ${JSON.stringify(requestBody.inputData, null, 2)}`;
 
     /**
